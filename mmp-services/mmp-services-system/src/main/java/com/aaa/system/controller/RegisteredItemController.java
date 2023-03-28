756b1068358f4dca6b350340e5d30184ab51fcf6
liangmenglei <842117675@qq.com>
2023-03-28 16:06:28 +0800

    刘鸿飞需要接口提供

---
+++ mmp-services/mmp-services-system/src/main/java/com/aaa/system/controller/RegisteredItemController.java
@@ -0,0 +1,21 @@
+package com.aaa.system.controller;
+
+import com.aaa.core.entity.RegisteredItem;
+import com.aaa.system.dao.RegisteredItemDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("system/registeredItem")
+public class RegisteredItemController {
+    @Autowired
+    private RegisteredItemDao dao;
+    @GetMapping("getAll")
+    public List<RegisteredItem> getAll(){
+        return dao.selectList(null);
+    }
+}
---
+++ mmp-services/mmp-services-system/src/main/java/com/aaa/system/dao/RegisteredItemDao.java
@@ -0,0 +1,7 @@
+package com.aaa.system.dao;
+
+import com.aaa.core.entity.RegisteredItem;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface RegisteredItemDao extends BaseMapper<RegisteredItem> {
+}
